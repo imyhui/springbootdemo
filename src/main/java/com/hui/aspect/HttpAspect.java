ce63a8b874c1f4cd9b80f3689fb4a80cf11e6773
liuyonghui <1359158019@qq.com>
2018-05-12 23:10:40 +0800

    AOP统一处理请求日志

--- src/main/java/com/hui/aspect/HttpAspect.java
+++ src/main/java/com/hui/aspect/HttpAspect.java
@@ -1,11 +1,13 @@
 package com.hui.aspect;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by yz on 18/5/10.
@@ -19,19 +21,41 @@ public class HttpAspect {
 
     /**
      * 可先写方法再写注解，有自动补全
-     *定义切面代码复用
+     * 定义切面代码复用
      */
     @Pointcut("execution(public * com.hui.controller.GirlController.*(..))")
     public void log() {
     }
 
     @Before("log()")
-    public void doBefor() {
-        logger.info("拦截前");
+    public void doBefor(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        //url
+        logger.info("url={}", request.getRequestURL());
+
+        //method
+        logger.info("method={}", request.getMethod());
+
+        //ip
+        logger.info("ip={}", request.getRemoteAddr());
+
+
+        //类方法
+        logger.info("class_method={}",joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+
+        //参数
+        logger.info("args={}", joinPoint.getArgs());
     }
 
     @After("log()")
     public void doAfter() {
         logger.info("拦截后");
+    }
+
+    @AfterReturning(returning = "object", pointcut = "log()")
+    public void doAfterReturning(Object object){
+        logger.info("response={}",object.toString());
     }
 }
