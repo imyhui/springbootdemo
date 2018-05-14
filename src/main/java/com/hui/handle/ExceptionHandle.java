794be5a19101e4f7f8bba8dda34f37669d810c14
liuyonghui <1359158019@qq.com>
2018-05-15 01:03:31 +0800

    输出系统异常信息

--- src/main/java/com/hui/handle/ExceptionHandle.java
+++ src/main/java/com/hui/handle/ExceptionHandle.java
@@ -3,15 +3,20 @@ package com.hui.handle;
 import com.hui.domain.Result;
 import com.hui.exception.GirlException;
 import com.hui.utils.ResultUtil;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.logging.Logger;
 
 /**
  * Created by yz on 18/5/13.
  */
 @ControllerAdvice
 public class ExceptionHandle {
+
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(ExceptionHandle.class);
 
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
@@ -20,6 +25,7 @@ public class ExceptionHandle {
             GirlException girlException = (GirlException) e;
             return ResultUtil.error(girlException.getCode(),girlException.getMessage());
         } else {
+            logger.error("【系统异常】{}", e);
             return ResultUtil.error(-1,"未知错误");
         }
     }
